0ed81bc1f1fb58fbeba0bac5e1fbf539c6f98e69
Steven Kuhn <email@stevenkuhn.net>
2022-09-07 07:29:43 -0500

    Add publishing github release to ci build

--- build/Build.cs
+++ build/Build.cs
@@ -99,4 +99,79 @@ class Build : NukeBuild
                 .GlobFiles($"**/{Configuration}/**/Sknet.*.{GitVersion.SemVer}.*nupkg")
                 .ForEach(file => CopyFileToDirectory(file, ArtifactsDirectory));
         });
+
+    Target PublishToGitHub => _ => _
+        .DependsOn(PublishArtifacts)
+        .Requires(() => GitHubAccessToken)
+        .Executes(async () =>
+        {
+            Log.Debug($"Creating 'v{GitVersion.SemVer}' release in GitHub...");
+            var github = new Octokit.GitHubClient(new Octokit.ProductHeaderValue("sknet.openiddict.litedb.build"))
+            {
+                Credentials = new Octokit.Credentials(GitHubAccessToken)
+            };
+
+            Octokit.Release release = null;
+            try
+            {
+                Log.Information($"Retrieving existing release tagged as 'v{GitVersion.SemVer}'...");
+                release = await github.Repository.Release.Get(GitHubRepositoryOwner, GitHubRepositoryName, $"v{GitVersion.SemVer}");
+            }
+            catch (Octokit.NotFoundException)
+            {
+                Log.Information("Release not found. Retrieving existing draft release...");
+                var releases = await github.Repository.Release.GetAll(GitHubRepositoryOwner, GitHubRepositoryName);
+                release = releases.SingleOrDefault(r => r.Draft && (r.TagName == $"v{GitVersion.SemVer}" || r.TagName.StartsWith($"v{GitVersion.MajorMinorPatch}-{GitVersion.PreReleaseLabel}")));
+            }
+
+            if (release != null)
+            {
+                Log.Information($"Release '{release.Name}' found. Updating release...");
+                release = await github.Repository.Release.Edit(GitHubRepositoryOwner, GitHubRepositoryName, release.Id, new Octokit.ReleaseUpdate
+                {
+                    Name = $"v{GitVersion.SemVer}",
+                    TagName = $"v{GitVersion.SemVer}",
+                    Body = !string.IsNullOrWhiteSpace(release.Body)
+                        ? release.Body
+                        : $"Release notes for `v{GitVersion.SemVer}` are not available at this time.",
+                    Draft = release.Draft,
+                    Prerelease = !string.IsNullOrWhiteSpace(GitVersion.PreReleaseTag),
+                    TargetCommitish = GitRepository.Commit
+                });
+            }
+            else
+            {
+                Log.Information($"Release not found. Creating a new draft release...");
+                release = await github.Repository.Release.Create(GitHubRepositoryOwner, GitHubRepositoryName, new Octokit.NewRelease($"v{GitVersion.SemVer}")
+                {
+                    Name = $"v{GitVersion.SemVer}",
+                    Body = $"Release notes for `v{GitVersion.SemVer}` are not available at this time.",
+                    Draft = true,
+                    Prerelease = !string.IsNullOrWhiteSpace(GitVersion.PreReleaseTag),
+                    TargetCommitish = GitRepository.Commit
+                });
+            }
+
+            Log.Information("Removing existing assets (if any)...");
+            var assets = await github.Repository.Release.GetAllAssets(GitHubRepositoryOwner, GitHubRepositoryName, release.Id);
+            foreach (var asset in assets)
+            {
+                await github.Repository.Release.DeleteAsset(GitHubRepositoryOwner, GitHubRepositoryName, asset.Id);
+            }
+
+            var artifacts = ArtifactsDirectory.GlobFiles($"Sknet.*.{GitVersion.SemVer}.*");
+            foreach (var artifact in artifacts)
+            {
+                var file = new FileInfo(artifact);
+                using var stream = File.OpenRead(artifact);
+
+                Log.Information($"Uploading asset '{file.Name}'...");
+                var asset = await github.Repository.Release.UploadAsset(release, new Octokit.ReleaseAssetUpload()
+                {
+                    ContentType = "application/zip",
+                    FileName = file.Name,
+                    RawData = stream,
+                });
+            }
+        });
 }
