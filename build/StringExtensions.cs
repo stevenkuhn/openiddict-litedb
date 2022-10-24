1b5fdd94d3ab0f1bd85eca34e64c00fa2f48d764
Steven Kuhn <email@stevenkuhn.net>
2022-10-24 16:51:48 -0500

    Update licensing and project version reference

--- build/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-static class StringExtensions
-{
-    public static (string T1, string T2) Split(this string value, char separator)
-    {
-        var result = value.Split(separator, 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-        return (result[0], result.Length > 1 ? result[1] : string.Empty);
-    }
-}
